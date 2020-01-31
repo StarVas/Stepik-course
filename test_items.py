d04cabc890656ffcb560de707329336ecd647d2d
StarVas <vasyl.starykov@gmail.com>
2020-01-31 16:45:39 +0200

    small touch

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 import time
 
-def test_guest_should_see_login_link(browser):
+def test_add_to_cart_button_is_displayed(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     time.sleep(10)
